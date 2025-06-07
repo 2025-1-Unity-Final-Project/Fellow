bf379f048de9a6d7d4854947eefa923e75bdab58
Kyu hyunSung <dmp100@naver.com>
2025-06-07 10:53:29 +0900

    Change Scene to NPC

--- Assets/Script/SceneChanger.cs
+++ Assets/Script/SceneChanger.cs
@@ -3,20 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    // 전환할 씬 이름을 Inspector에서 지정할 수 있게 public으로 선언
     public string sceneName;
 
-    // 버튼 클릭 시 호출할 함수
     public void LoadScene()
     {
         if (!string.IsNullOrEmpty(sceneName))
         {
             SceneManager.LoadScene(sceneName);
-            Debug.Log("씬 전환: " + sceneName);
+            Debug.Log("Scene Changer: " + sceneName);
         }
         else
         {
-            Debug.LogWarning("SceneChanger: sceneName이 설정되어 있지 않습니다.");
+            Debug.LogWarning("Scene Changer: sceneName is not set.");
         }
     }
 }
